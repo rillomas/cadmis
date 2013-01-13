3355e26ad640f910bac052faedeb0e1a66b0f0a1
Masato Hori <rillomas@gmail.com>
2013-01-13 14:53:18 +0900

    initial commit

---
+++ cadmis/cadmis.go
@@ -0,0 +1,49 @@
+package cadmis
+
+import (
+	"appengine"
+	"appengine/user"
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", hostStaticFile)
+	http.HandleFunc("/_ah/login_required", loginHandler)
+}
+
+func hostStaticFile() {
+	path := req.URL.Path[1:]
+	fmt.Printf("path: %s\n",path)
+	http.ServeFile(w, req, path)	
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+	   fmt.Fprintf(w, "Please, <a href='/_ah/login_required'>login</a>.")
+	} else {
+		url, _ := user.LogoutURL(c, "/")
+		fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
+	}
+}
+
+
+func loginHandler(w http.ResponseWriter, r * http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	providers := map[string]string {
+		"Google"   : "gmail.com",
+	}
+
+	c := appengine.NewContext(r)
+	fmt.Fprintf(w, "Sign in at: ")
+	for name, url := range providers {
+		login_url, err := user.LoginURLFederated(c, "/", url)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "[<a href='%s'>%s</a>]", login_url, name)
+	}
+}
