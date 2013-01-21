e051185e593532b96af1cbe6479d82da4e06d60c
Noriyuki Futatsugi <noli@futatsugi.net>
2013-01-22 00:48:31 +0900

    modified:   compute.py

--- vm/compute.py
+++ vm/compute.py
@@ -8,16 +8,20 @@ from wsgiref.simple_server import make_server
 COMMAND = "ssh 180.37.181.90 ls -l %s"
 COMMAND_COPY = "scp %s 180.37.181.90:/home/futatsugi/develop/work/challengers/2013_winter/hackathon/svm/"
 COMMAND_TRAIN = "ssh 180.37.181.90 /home/futatsugi/bin/svm-train -c 16 -g 4 -m 400 /home/futatsugi/develop/work/challengers/2013_winter/hackathon/svm/%s /home/futatsugi/develop/work/challengers/2013_winter/hackathon/svm/%s"
-COMMAND_SCORE_COPY = "scp %s 180.37.181.90:/home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/"
-COMMAND_SCORE = 'ssh 180.37.181.90 "setenv LD_LIBRARY_PATH /usr/local/tbb/build/linux_intel64_gcc_cc4.4.6_libc2.12_kernel2.6.32_release;/home/futatsugi/bin/scoring_tbb /home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/%s"'
+#COMMAND_SCORE_COPY = "scp %s 180.37.181.90:/home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/"
+#COMMAND_SCORE_COPY = "cp %s /home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/"
+#COMMAND_SCORE = 'ssh 180.37.181.90 "setenv LD_LIBRARY_PATH /usr/local/tbb/build/linux_intel64_gcc_cc4.4.6_libc2.12_kernel2.6.32_release;/home/futatsugi/bin/scoring_tbb /home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/%s"'
+#COMMAND_SCORE = "setenv LD_LIBRARY_PATH /usr/local/tbb/build/linux_intel64_gcc_cc4.4.6_libc2.12_kernel2.6.32_release;/home/futatsugi/bin/scoring_tbb %s"
+COMMAND_SCORE = "export LD_LIBRARY_PATH=/usr/local/tbb/build/linux_intel64_gcc_cc4.4.6_libc2.12_kernel2.6.32_release;/home/futatsugi/bin/scoring_tbb %s"
+#COMMAND_SCORE = "/home/futatsugi/bin/scoring_tbb %s"
 
-DATAFILE = "score.dat"
+DATAFILE = "/home/futatsugi/develop/work/challengers/2013_winter/hackathon/scoring/score.dat"
 
 def compute(environ, start_response):
 	result = ""
 	if environ["PATH_INFO"] == "/":
-		result = subprocess.Popen(COMMAND % (".",), shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
-		result = result.replace("\n", "<br />")
+		#result = subprocess.Popen(COMMAND % (".",), shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+		#result = result.replace("\n", "<br />")
 		if environ["REQUEST_METHOD"] == "POST":
 			fs = cgi.FieldStorage(fp=environ["wsgi.input"], environ=environ, keep_blank_values=1)
 			user = fs.getfirst("user", "").strip()
@@ -38,7 +42,7 @@ def compute(environ, start_response):
 				#f.write(" ".join(map(str, datalist)))
 				print >>f, " ".join(map(str, datalist))
 			f.close()
-			os.system(COMMAND_SCORE_COPY % DATAFILE)
+			#os.system(COMMAND_SCORE_COPY % DATAFILE)
 			#print user, data #####
 			result = subprocess.Popen(COMMAND_SCORE % (DATAFILE,), shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
 			result = result.strip().split("\n")
