2d3cf07f6b588615e34d73a5a119c4768ed58681
Samuel Audet <samuel.audet@gmail.com>
2013-01-22 02:38:50 +0900

    Renamed problemEngine.go to examSelection.go, changing its vocation

--- cadmis/exam.go
+++ cadmis/exam.go
@@ -42,16 +42,16 @@ type StoreExamResultRequest struct {
 }
 
 // 問題
-// type Problem struct {
-// 	Id    int64
-// 	Score int32
-// }
+ type Problem struct {
+ 	Id    int64
+ 	Score int32
+ }
 
 // 試験
-// type Exam struct {
-// 	Id          int64
-// 	ProblemList []*datastore.Key
-// }
+ type Exam struct {
+ 	Id          int64
+ 	ProblemList []*datastore.Key
+ }
 
 // 問題の統計情報
 // type ProblemStatistics struct {
---
+++ cadmis/examSelection.go
@@ -0,0 +1,293 @@
+package cadmis
+
+import (
+	"appengine"
+	"fmt"
+//    "io"
+    "io/ioutil"
+	// "appengine/user"
+	"appengine/datastore"
+    "appengine/urlfetch"
+	// "code.google.com/p/go.crypto/bcrypt"
+	"encoding/json"
+	// "io/ioutil"
+	"net/http"
+	"strconv"
+    // "strings"
+	// "time"
+)
+
+type Rank struct {
+    ProblemId int64
+    UserId    int64
+    Score     int32
+}
+
+func handleComputeRank(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    uid, err1 := strconv.Atoi(r.FormValue("userId"));
+    pid, err2 := strconv.Atoi(r.FormValue("examId"));
+    if err1 != nil {
+        uid = -1;
+    }
+    if err2 != nil {
+        pid = -1;
+    }
+    score, err3 := computeRank(c, int64(uid), int64(pid))
+    if err3 != nil {
+        http.Error(w, err3.Error(), http.StatusInternalServerError)
+        return;
+    }
+    out, _ := json.Marshal(score)
+    fmt.Fprint(w, string(out))
+}
+
+func handleInitExams(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+
+/*
+    client := urlfetch.Client(c)
+    resp, err := client.Get("http://api.iknow.jp/categories")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        //return err
+    }
+    defer resp.Body.Close()
+    body, err2 := ioutil.ReadAll(resp.Body)
+    if err2 != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        //return err
+    }
+    //dec := json.NewDecoder(strings.NewReader(stream));
+    //dec := json.NewDecoder(resp.Body)
+    var categories []map[string]interface{}
+    err3 := json.Unmarshal(body, &categories)
+    if err3 != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        //return err
+    }
+    for i := range categories {
+        fmt.Fprintf(w, "%s: %s\n", categories[i]["id"], categories[i]["name"])
+    }
+*/
+    client := urlfetch.Client(c)
+    resp, err := client.Get("http://api.iknow.jp/goals/")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    defer resp.Body.Close()
+    body, err2 := ioutil.ReadAll(resp.Body)
+    if err2 != nil {
+        http.Error(w, err2.Error(), http.StatusInternalServerError)
+    }
+    var goals1 map[string]interface{}
+    err3 := json.Unmarshal(body, &goals1)
+    if err3 != nil {
+        http.Error(w, err3.Error(), http.StatusInternalServerError)
+    }
+    var goals2 []interface{}
+    goals2 = goals1["goals"].([]interface{})
+    for i := range goals2 {
+        //fmt.Fprintf(w, "%d: %s\n", int(items2[i]["id"].(float64)), items2[i]["uri"])
+
+        var exam Exam
+        exam.Id = int64(goals2[i].(map[string]interface{})["id"].(float64))
+        //exam.Category = "English";
+// /*
+        var er ExamResult
+        if i < 5 {
+            er.UserId = 42 // + int64(i)
+            er.ExamId = exam.Id
+        }
+// */
+        client := urlfetch.Client(c)
+        resp, err := client.Get("http://api.iknow.jp/goals/" + strconv.Itoa(int(exam.Id)) + "/items")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        defer resp.Body.Close()
+        body, err2 := ioutil.ReadAll(resp.Body)
+        if err2 != nil {
+            http.Error(w, err2.Error(), http.StatusInternalServerError)
+            return
+        }
+        var items map[string]interface{}
+        err3 := json.Unmarshal(body, &items)
+        if err3 != nil {
+            http.Error(w, err3.Error(), http.StatusInternalServerError)
+            return
+        }
+        if items["items"] == nil {
+            continue
+        }
+        items2 := items["items"].([]interface{})
+        for j := range items2 {
+            var problem Problem
+            problem.Id = int64(items2[j].(map[string]interface{})["id"].(float64))
+            problem.Score = 10;
+            //fmt.Fprintf(w, "%d: %s\n", int(items2[j]["id"].(float64)), items2[j]["uri"])
+            key, err2 := datastore.Put(c, datastore.NewIncompleteKey(c, "Problem", nil), &problem)
+            if err2 != nil {
+                http.Error(w, err2.Error(), http.StatusInternalServerError)
+                return
+            }
+            exam.ProblemList = append(exam.ProblemList, key)
+// /*
+            if i < 5 {
+                var pr ProblemResult
+                pr.ProblemId = problem.Id;
+                if i < 7 {
+                    pr.Correct = true;
+                } else {
+                    pr.Correct = false;
+                }
+                key, err2 := datastore.Put(c, datastore.NewIncompleteKey(c, "ProblemResult", nil), &pr)
+                if err2 != nil {
+                    http.Error(w, err2.Error(), http.StatusInternalServerError)
+                    return
+                }
+                er.ProblemList = append(er.ProblemList, key)
+            }
+// */
+        }
+        _, err4 := datastore.Put(c, datastore.NewIncompleteKey(c, "Exam", nil), &exam)
+        if err4 != nil {
+            http.Error(w, err4.Error(), http.StatusInternalServerError)
+            return
+        }
+// /*
+        if i < 5 {
+            _, err5 := datastore.Put(c, datastore.NewIncompleteKey(c, "ExamResult", nil), &er)
+            if err5 != nil {
+                http.Error(w, err5.Error(), http.StatusInternalServerError)
+                return
+            }
+        }
+// */
+    }
+}
+
+func getExamResultScore(c appengine.Context, e ExamResult) (error, int32) {
+    var score int32
+    for i := range e.ProblemList {
+        pr := new(ProblemResult)
+        if err := datastore.Get(c, e.ProblemList[i], pr); err != nil {
+            return err, score
+        }
+        if (pr.Correct) {
+            q := datastore.NewQuery("Problem").Filter("Id =", pr.ProblemId).Limit(1)
+            problems := make([]Problem, 0, 1)
+            if _, err := q.GetAll(c, &problems); err != nil {
+                return err, score
+            }
+            score += problems[0].Score
+        }
+    }
+    return nil, score
+}
+
+func getExamScore(c appengine.Context, e Exam) (error, int32) {
+    var score int32
+    for i := range e.ProblemList {
+        p := new(Problem)
+        if err := datastore.Get(c, e.ProblemList[i], p); err != nil {
+            return err, score
+        }
+        score += p.Score
+    }
+    return nil, score
+}
+
+func computeRank(c appengine.Context, userId int64, examId int64) ([]Rank, error) {
+    score := make([]Rank, 0, 0)
+    q := datastore.NewQuery("ExamResult")
+    for t := q.Run(c); ; {
+        var e ExamResult;
+        _, err := t.Next(&e)
+        if err == datastore.Done {
+            break;
+        }
+        if err != nil {
+            return score, err
+        }
+        if (userId == -1 || e.UserId == userId) && (examId == -1 || e.ExamId == examId) {
+            err, s := getExamResultScore(c, e);
+            if err != nil {
+                return score, err
+            }
+            score = append(score, Rank{e.ExamId, e.UserId, s})
+        }
+    }
+    return score, nil
+}
+
+func handleGetExam(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    uid, err1 := strconv.Atoi(r.FormValue("userId"));
+    if err1 != nil {
+        http.Error(w, err1.Error(), http.StatusInternalServerError)
+        return;
+    }
+
+    var entries []ExamResult
+    var score int32
+    score = 0;
+    q := datastore.NewQuery("ExamResult").Filter("UserId =", uid)
+    if _, err := q.GetAll(c, &entries); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    for i := range entries {
+        err, s := getExamResultScore(c, entries[i]);
+        if err != nil {
+            return
+        }
+        score += s;
+    }
+    if len(entries) > 0 {
+        score = score / int32(len(entries))
+    }
+    score += 10 // boost level up!
+
+    var exams []Exam
+    q2 := datastore.NewQuery("Exam")
+    if _, err := q2.GetAll(c, &exams); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    var newExam Exam
+    for i := range exams {
+        var results []ExamResult
+        q := datastore.NewQuery("ExamResult").Filter("UserId =", uid).Filter("ExamId =", exams[i].Id).Limit(1)
+        if _, err := q.GetAll(c, &results); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        err1, diff1 := getExamScore(c, exams[i]);
+        err2, diff2 := getExamScore(c, newExam);
+        if err1 != nil {
+            http.Error(w, err1.Error(), http.StatusInternalServerError)
+            return
+        }
+        if err2 != nil {
+            http.Error(w, err2.Error(), http.StatusInternalServerError)
+            return
+        }
+        diff1 -= score
+        diff2 -= score
+        if diff1 < 0 {
+            diff1 = -diff1
+        }
+        if diff2 < 0 {
+            diff2 = -diff2;
+        }
+        if len(results) == 0 && (i == 0 || diff1 < diff2) {
+            newExam = exams[i]
+            break
+        }
+    }
+    out, _ := json.Marshal(newExam.Id)
+    fmt.Fprint(w, string(out))
+}
+
